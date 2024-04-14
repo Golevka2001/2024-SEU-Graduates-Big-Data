dd0975fdb78433145498498b4e705e76f3a548bc
Gol3vka <gol3vka@163.com>
2024-04-14 23:38:53 +0800

    feat: add template showing stats

--- stat_data/views.py
+++ stat_data/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 
+from stat_data.models import GraduatePersonalStat
+
 
 def stat_data_view(request):
-    return render(request, "stat_data.html")
+    seu_card_id = "213216666"  # TODO: 测试用，后续删除
+    graduate = GraduatePersonalStat.objects.get(seu_card_id=seu_card_id)
+    return render(request, "stat_data.html", {"graduate": graduate})
