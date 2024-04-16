20e3e25a5df1541ae6551c15ff50d80325045b69
Gol3vka <gol3vka@163.com>
2024-04-16 09:13:14 +0800

    fix: adjust databases and models

--- stat_data/models.py
+++ stat_data/models.py
@@ -15,132 +15,179 @@ class GraduatePersonalStat(models.Model):
     # 一卡通号
     seu_card_id = models.CharField(max_length=20, primary_key=True, verbose_name="一卡通号", unique=True)
     # 学号
-    student_id = models.CharField(max_length=20, verbose_name="学号", null=True, blank=True)
+    student_id = models.CharField(max_length=20, verbose_name="学号")
     # 姓名
-    full_name = models.CharField(max_length=20, verbose_name="姓名", null=True, blank=True)
+    full_name = models.CharField(max_length=20, verbose_name="姓名")
     # 学院
-    unit_name = models.CharField(max_length=20, verbose_name="学院", null=True, blank=True)
+    unit_name = models.CharField(max_length=20, verbose_name="学院")
     # 专业
-    major = models.CharField(max_length=20, verbose_name="专业", null=True, blank=True)
+    major = models.CharField(max_length=20, verbose_name="专业")
     # 宿舍名称
-    dormitory_name = models.CharField(max_length=20, verbose_name="宿舍名称", null=True, blank=True)
+    dormitory_name = models.CharField(max_length=20, verbose_name="宿舍名称")
     # 入学时间
-    enroll_date = models.DateField(verbose_name="入学时间", null=True, blank=True)
+    enroll_date = models.DateField(verbose_name="入学时间")
     # 毕业生类型: 0-本科毕业生; 1-硕士毕业生; 2-博士毕业生
-    # graduate_type = models.IntegerField(choices=GRADUATE_TYPE_CHOICES, verbose_name="毕业生类型", null=True, blank=True)
-    # 出生日期
-    # birth_date = models.DateField(verbose_name="出生日期", null=True, blank=True)
-    # 相同出生日期的毕业生人数
-    # same_birth_date_num = models.IntegerField(verbose_name="相同出生日期的毕业生人数", null=True, blank=True)
+    # graduate_type = models.IntegerField(choices=GRADUATE_TYPE_CHOICES, verbose_name="毕业生类型")
+    # 出生日期（月日，例如：0501）
+    # birth_date = models.CharField(max_length=5, verbose_name="出生日期（月日）")
     # 生源地
-    # origin = models.CharField(max_length=50, verbose_name="生源地", null=True, blank=True)
-    # 相同生源地的毕业生人数
-    # same_origin_num = models.IntegerField(verbose_name="相同生源地的毕业生人数", null=True, blank=True)
+    # origin = models.CharField(max_length=50, verbose_name="生源地")
 
     # ---------- 网络 ---------- #
     # 网络在线天数
-    network_online_days = models.CharField(max_length=10, verbose_name="网络在线天数", null=True, blank=True)
+    network_online_days = models.CharField(max_length=10, verbose_name="网络在线天数")
     # 网络总流量（GB）
-    network_flow = models.CharField(max_length=20, verbose_name="网络总流量(GB)", null=True, blank=True)
-    # 流量相当于看了多少甄嬛传（按 167GB 计算）
-    # network_flow_equivalence = models.FloatField(verbose_name="流量相当于看了多少甄嬛传", null=True, blank=True)
+    network_flow = models.CharField(max_length=20, verbose_name="网络总流量(GB)")
 
     # ---------- 食堂 ---------- #
     # 消费次数
-    consumption_times = models.CharField(max_length=10, verbose_name="消费次数", null=True, blank=True)
+    consumption_times = models.CharField(max_length=10, verbose_name="消费次数")
     # 消费金额
-    consumption_amount = models.CharField(max_length=10, verbose_name="消费金额", null=True, blank=True)
+    consumption_amount = models.CharField(max_length=10, verbose_name="消费金额")
     # 最频繁消费地点
-    most_frequent_consumption_place = models.CharField(max_length=20, verbose_name="最频繁消费地点", null=True,
-                                                       blank=True)
+    most_frequent_consumption_place = models.CharField(max_length=20, verbose_name="最频繁消费地点")
     # 最高单次消费
-    highest_single_consumption_amount = models.CharField(max_length=10, verbose_name="最高单次消费", null=True,
-                                                         blank=True)
+    highest_single_consumption_amount = models.CharField(max_length=10, verbose_name="最高单次消费")
     # 最高单次消费日期
-    highest_single_consumption_date = models.DateField(verbose_name="最高单次消费日期", null=True, blank=True)
-    # 最常去相同食堂的人数百分比
-    # same_canteen_percentage = models.CharField(max_length=10, verbose_name="去相同食堂的人数百分比", null=True, blank=True)
+    highest_single_consumption_date = models.DateField(verbose_name="最高单次消费日期")
+    # 【calc】最常去相同食堂的人数百分比
+    # same_canteen_percentage = models.CharField(max_length=10, verbose_name="去相同食堂的人数百分比")
     # 最频繁同时、同地消费的人
-    # canteen_friend = models.CharField(max_length=20, verbose_name="最频繁同时、同地消费的人", null=True, blank=True)
+    # canteen_friend = models.CharField(max_length=20, verbose_name="最频繁同时、同地消费的人")
     # 同时、同地消费的次数
-    # canteen_friend_together_times = models.CharField(max_length=10, verbose_name="同时、同地消费的次数", null=True, blank=True)
+    # canteen_friend_together_times = models.CharField(max_length=10, verbose_name="同时、同地消费的次数")
 
     # ---------- 课程 ---------- #
     # 第一节课日期
-    first_class_date = models.DateField(verbose_name="第一节课日期", null=True, blank=True)
+    first_class_date = models.DateField(verbose_name="第一节课日期")
     # 第一节课地点
-    first_class_place = models.CharField(max_length=20, verbose_name="第一节课地点", null=True, blank=True)
+    first_class_place = models.CharField(max_length=20, verbose_name="第一节课地点")
     # 第一节课名称
-    first_class_name = models.CharField(max_length=20, verbose_name="第一节课名称", null=True, blank=True)
+    first_class_name = models.CharField(max_length=20, verbose_name="第一节课名称")
     # 第一节课教师姓名
-    first_class_teacher = models.CharField(max_length=20, verbose_name="第一节课教师姓名", null=True, blank=True)
+    first_class_teacher = models.CharField(max_length=20, verbose_name="第一节课教师姓名")
     # 选课门数
-    course_selected_num = models.CharField(max_length=5, verbose_name="选课门数", null=True, blank=True)
+    course_selected_num = models.CharField(max_length=5, verbose_name="选课门数")
     # 获得学分
-    credits_obtained = models.CharField(max_length=5, verbose_name="获得学分", null=True, blank=True)
+    credits_obtained = models.CharField(max_length=5, verbose_name="获得学分")
     # 最高分课程名
-    highest_score_course = models.CharField(max_length=20, verbose_name="最高分课程名", null=True, blank=True)
+    highest_score_course = models.CharField(max_length=20, verbose_name="最高分课程名")
     # 论文数量
-    papers_num = models.CharField(max_length=5, verbose_name="论文数量", null=True, blank=True)
+    papers_num = models.CharField(max_length=5, verbose_name="论文数量")
 
     # ---------- 人文讲座 ---------- #
     # 参加讲座次数
-    lecture_attended_times = models.CharField(max_length=5, verbose_name="参加讲座次数", null=True, blank=True)
+    lecture_attended_times = models.CharField(max_length=5, verbose_name="参加讲座次数")
     # 第一次参加讲座名称
-    first_lecture_name = models.CharField(max_length=100, verbose_name="第一次参加讲座名称", null=True, blank=True)
+    first_lecture_name = models.CharField(max_length=100, verbose_name="第一次参加讲座名称")
     # 第一次参加讲座日期
-    first_lecture_date = models.DateField(verbose_name="第一次参加讲座日期", null=True, blank=True)
+    first_lecture_date = models.DateField(verbose_name="第一次参加讲座日期")
 
     # ---------- SRTP ---------- #
     # SRTP项目数
-    srtp_project_num = models.CharField(max_length=5, verbose_name="SRTP项目数", null=True, blank=True)
+    srtp_project_num = models.CharField(max_length=5, verbose_name="SRTP项目数")
     # SRTP学分
-    srtp_score = models.CharField(max_length=10, verbose_name="SRTP学分", null=True, blank=True)
+    srtp_score = models.CharField(max_length=10, verbose_name="SRTP学分")
 
     # ---------- 志愿活动 ---------- #
     # 志愿活动数量
-    volunteer_activity_num = models.CharField(max_length=5, verbose_name="志愿活动数量", null=True, blank=True)
+    volunteer_activity_num = models.CharField(max_length=5, verbose_name="志愿活动数量")
     # 志愿活动时长
-    volunteer_duration = models.CharField(max_length=10, verbose_name="志愿活动时长", null=True, blank=True)
+    volunteer_duration = models.CharField(max_length=10, verbose_name="志愿活动时长")
 
     # ---------- 社会实践 ---------- #
     # 实践项目数量
-    practice_project_num = models.CharField(max_length=5, verbose_name="实践项目数量", null=True, blank=True)
+    practice_project_num = models.CharField(max_length=5, verbose_name="实践项目数量")
     # 第一个实践项目
-    first_practice_project_name = models.CharField(max_length=50, verbose_name="第一个实践项目", null=True, blank=True)
+    first_practice_project_name = models.CharField(max_length=50, verbose_name="第一个实践项目")
     # 第一个实践项目队员（非本人的另一人姓名）
     first_practice_project_member = models.CharField(max_length=20,
-                                                     verbose_name="第一个实践项目队员（非本人的另一人姓名）", null=True,
-                                                     blank=True)
+                                                     verbose_name="第一个实践项目队员（非本人的另一人姓名）")
 
     # ---------- 图书馆 ---------- #
     # 总借阅量
-    total_borrowed_books_num = models.CharField(max_length=10, verbose_name="总借阅量", null=True, blank=True)
+    total_borrowed_books_num = models.CharField(max_length=10, verbose_name="总借阅量")
     # 最长借阅天数
-    longest_book_borrowing_days = models.CharField(max_length=5, verbose_name="最长借阅天数", null=True, blank=True)
+    longest_book_borrowing_days = models.CharField(max_length=5, verbose_name="最长借阅天数")
     # 个人借阅书籍中总借阅次数最少书名
-    nice_book_name = models.CharField(max_length=50, verbose_name="个人借阅书籍中总借阅次数最少书名", null=True,
-                                      blank=True)
-    # 借阅量百分比排名
-    # book_borrowing_percentage_rank = models.CharField(max_length=10, verbose_name="借阅量百分比排名", null=True, blank=True)
+    nice_book_name = models.CharField(max_length=50, verbose_name="个人借阅书籍中总借阅次数最少书名")
+    # 【calc】是否显示借阅量排名
+    # show_borrowing_rank = models.BooleanField(default=False, verbose_name="是否显示借阅量排名")
+    # 【calc】借阅量百分比排名
+    # book_borrowing_percentage_rank = models.CharField(max_length=10, verbose_name="借阅量百分比排名")
 
     # ---------- 体育 ---------- #
     # 场馆预约次数
-    gym_ordered_times = models.CharField(max_length=5, verbose_name="场馆预约次数", null=True, blank=True)
+    gym_ordered_times = models.CharField(max_length=5, verbose_name="场馆预约次数")
     # 首次预约场馆
-    first_ordered_gym = models.CharField(max_length=20, verbose_name="首次预约场馆", null=True, blank=True)
+    first_ordered_gym = models.CharField(max_length=20, verbose_name="首次预约场馆")
     # 首次预约日期
-    first_ordered_date = models.DateField(verbose_name="首次预约日期", null=True, blank=True)
+    first_ordered_date = models.DateField(verbose_name="首次预约日期")
     # 跑操次数
-    morning_exercise_times = models.CharField(max_length=5, verbose_name="跑操次数", null=True, blank=True)
+    morning_exercise_times = models.CharField(max_length=5, verbose_name="跑操次数")
     # 最早跑操打卡时间
-    earliest_exercise_time = models.TimeField(verbose_name="最早跑操打卡时间", null=True, blank=True)
+    earliest_exercise_time = models.TimeField(verbose_name="最早跑操打卡时间")
 
     def __str__(self):
         return f"{self.full_name}[{self.seu_card_id}]"
 
     class Meta:
-        db_table = "graduate_personal_stat"
+        db_table = "gbd_graduate_personal_stat"
         verbose_name = "毕业生个人统计信息表"
         verbose_name_plural = verbose_name
         ordering = ["seu_card_id"]
+
+
+class BirthDateStat(models.Model):
+    """毕业生出生日期统计表"""
+    # 出生日期（月日，例如：0501）
+    birth_date = models.DateField(primary_key=True, unique=True, verbose_name="出生日期（月日）")
+    # 此出生日期的毕业生人数
+    count = models.IntegerField(verbose_name="此出生日期的毕业生人数")
+
+    class Meta:
+        db_table = "gbd_birth_date_stat"
+        verbose_name = "毕业生出生日期统计表"
+        verbose_name_plural = verbose_name
+        ordering = ["birth_date"]
+
+
+class OriginStat(models.Model):
+    """毕业生生源地统计表"""
+    # 生源地
+    origin = models.CharField(max_length=50, primary_key=True, unique=True, verbose_name="生源地")
+    # 此生源地的毕业生人数
+    count = models.IntegerField(verbose_name="此生源地的毕业生人数")
+
+    class Meta:
+        db_table = "gbd_origin_stat"
+        verbose_name = "毕业生生源地统计表"
+        verbose_name_plural = verbose_name
+        ordering = ["origin"]
+
+
+class FavoriteCanteenStat(models.Model):
+    """毕业生最喜欢的食堂统计表"""
+    # 食堂名称
+    canteen_name = models.CharField(max_length=20, primary_key=True, unique=True, verbose_name="食堂名称")
+    # 此食堂的毕业生人数
+    count = models.IntegerField(verbose_name="此食堂的毕业生人数")
+
+    class Meta:
+        db_table = "gbd_favorite_canteen_stat"
+        verbose_name = "毕业生最喜欢的食堂统计表"
+        verbose_name_plural = verbose_name
+        ordering = ["canteen_name"]
+
+class LibraryBorrowingStat(models.Model):
+    """图书馆借阅数据表"""
+    # 学院
+    unit_name = models.CharField(max_length=20, primary_key=True, verbose_name="学院")
+    # 借阅量在学院中的排名
+    rank = models.IntegerField(verbose_name="借阅量在学院中的排名")
+    # 一卡通号
+    seu_card_id = models.CharField(max_length=20, verbose_name="一卡通号")
+    # 姓名
+    full_name = models.CharField(max_length=20, verbose_name="姓名")
+    # 总借阅量
+    total_borrowed_books_num = models.CharField(max_length=10, verbose_name="总借阅量")
--- stat_data/views.py
+++ stat_data/views.py
@@ -1,9 +1,36 @@
 from django.shortcuts import render
 
-from stat_data.models import GraduatePersonalStat
+from stat_data.models import (GraduatePersonalStat, BirthDateStat, OriginStat,
+                              LibraryBorrowingStat)
 
 
 def stat_data_view(request):
+    # 当前所查询毕业生的一卡通号
     seu_card_id = "213216666"  # TODO: 测试用，后续删除
     graduate = GraduatePersonalStat.objects.get(seu_card_id=seu_card_id)
-    return render(request, "stat_data.html", {"graduate": graduate})
+
+    # 相同出生月日的毕业生人数
+    birth_date = graduate.birth_date
+    same_birth_date_cnt = BirthDateStat.objects.get(birth_date=birth_date).count - 1
+
+    # 相同生源地的毕业生人数
+    origin = graduate.origin
+    same_origin_cnt = OriginStat.objects.get(origin=origin).count - 1
+
+    # 流量相当于看了多少甄嬛传（按 167GB 计算）
+    network_flow_equivalence = float(graduate.network_flow) / 167
+
+    # 全校最高借阅量（可以写成常量）
+    # max_total_borrowed_books_num = LibraryBorrowingStat.objects.order_by("-total_borrowed_books_num")[0]
+    max_total_borrowed_books_num = 123
+
+    # 本学院借阅量第一名
+    unit_name = graduate.unit_name
+    top_borrower_in_unit = LibraryBorrowingStat.objects.filter(unit_name=unit_name).order_by("-borrow_times")[0]
+
+    return render(request, "stat_data.html", {"graduate": graduate,
+                                              "same_birth_date_cnt": same_birth_date_cnt,
+                                              "same_origin_cnt": same_origin_cnt,
+                                              "network_flow_equivalence": network_flow_equivalence,
+                                              "max_total_borrowed_books_num": max_total_borrowed_books_num,
+                                              "top_borrower_in_unit": top_borrower_in_unit})
