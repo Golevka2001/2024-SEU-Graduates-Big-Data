790c8023a3f22f29418998eb5c982d4e97a43496
Gol3vka <gol3vka@163.com>
2024-04-17 18:10:58 +0800

    feat: sqlite -> mysql

--- graduates_big_data/settings.py
+++ graduates_big_data/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -73,8 +74,10 @@ WSGI_APPLICATION = "graduates_big_data.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "graduates_big_data.db",
+        "ENGINE": "django.db.backends.mysql",
+        'OPTIONS': {
+            'read_default_file': os.path.join(BASE_DIR, 'my.cnf'),
+        },
     },
 }
 
