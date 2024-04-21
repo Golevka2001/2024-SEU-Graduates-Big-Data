531b4dab4b2957da36f302d840522dc79e0ec8fa
Gol3vka <gol3vka@163.com>
2024-04-21 23:10:23 +0800

    doc: html text content

--- stat_data/views/personal_view.py
+++ stat_data/views/personal_view.py
@@ -8,6 +8,9 @@ from stat_data.models import BirthDateStat, FavoriteCanteenStat, GraduatePersona
 # 只有借阅量排名在前 50% 的毕业生才会显示借阅量排名
 SHOW_BORROWING_RANK_THRESHOLD = 0.5
 
+# 只有场馆预约次数排名在前 50% 的毕业生才会显示场馆预约次数排名
+SHOW_GYM_ORDERING_RANK_THRESHOLD = 0.5
+
 
 @login_required
 def personal_view(request):
@@ -42,6 +45,24 @@ def personal_view(request):
         canteen_name=graduate.most_frequent_consumption_place).count - 1
     same_favorite_canteen_percent = f'{same_favorite_canteen_cnt / total_graduate_cnt:.2%}'
 
+    # 是否显示最频繁同时、同地消费的人
+    show_canteen_friend = bool(graduate.canteen_friend)
+
+    # 是否显示论文
+    show_papers = int(graduate.papers_num) > 0
+
+    # 是否显示讲座
+    show_lectures = bool(graduate.first_lecture_name)
+
+    # 是否显示SRTP
+    show_srtp_projects = float(graduate.srtp_score) > 0
+
+    # 是否显示志愿活动
+    show_volunteer_activities = float(graduate.volunteer_duration) > 0
+
+    # 是否显示社会实践
+    show_practice_projects = bool(graduate.first_practice_project_name)
+
     # 借阅量排名（total_borrowed_books_num）
     borrowing_rank = GraduatePersonalStat.objects.filter(
         total_borrowed_books_num__gt=graduate.total_borrowed_books_num).count()
@@ -57,6 +78,15 @@ def personal_view(request):
     top_borrower_in_unit = \
         LibraryBorrowingStat.objects.filter(unit_name=unit_name).order_by("-total_borrowed_books_num")[0]
 
+    # 是否显示借阅详情
+    show_borrowing_detail = int(graduate.total_borrowed_books_num) > 0
+
+    # 场馆预约次数排名（gym_ordered_times）
+    gym_ordering_rank = GraduatePersonalStat.objects.filter(
+        gym_ordered_times__gt=graduate.gym_ordered_times).count()
+    show_gym_ordering_rank = gym_ordering_rank < total_graduate_cnt * SHOW_GYM_ORDERING_RANK_THRESHOLD
+    gym_ordering_rank_percent = f'{1 - gym_ordering_rank / total_graduate_cnt:.2%}'
+
     # 在学校的第多少天
     days_in_seu = (datetime.date.today() - graduate.enroll_date).days
 
@@ -65,8 +95,17 @@ def personal_view(request):
                                                   "same_origin_cnt": same_origin_cnt,
                                                   "network_flow_equivalence": network_flow_equivalence,
                                                   "same_favorite_canteen_percent": same_favorite_canteen_percent,
+                                                  "show_canteen_friend": show_canteen_friend,
+                                                  "show_papers": show_papers,
+                                                  "show_lecture": show_lectures,
+                                                  "show_srtp": show_srtp_projects,
+                                                  "show_volunteer_activities": show_volunteer_activities,
+                                                  "show_practice_projects": show_practice_projects,
                                                   "show_borrowing_rank": show_borrowing_rank,
                                                   "borrowing_rank_percent": borrowing_rank_percent,
                                                   "max_total_borrowed_books_num": max_total_borrowed_books_num,
-                                                  "days_in_seu": days_in_seu,
-                                                  "top_borrower_in_unit": top_borrower_in_unit})
+                                                  "top_borrower_in_unit": top_borrower_in_unit,
+                                                  "show_borrowing_detail": show_borrowing_detail,
+                                                  "show_gym_ordering_rank": show_gym_ordering_rank,
+                                                  "gym_ordering_rank_percent": gym_ordering_rank_percent,
+                                                  "days_in_seu": days_in_seu})
