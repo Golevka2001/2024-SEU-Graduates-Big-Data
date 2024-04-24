3d185a2ae694ce64a8bb5a0b4c88d998d45be838
Gol3vka <gol3vka@163.com>
2024-04-24 18:05:23 +0800

    fix: sync modifications of the database

--- stat_data/models/graduate_personal_stat.py
+++ stat_data/models/graduate_personal_stat.py
@@ -37,95 +37,99 @@ class GraduatePersonalStat(models.Model):
 
     # ---------- 网络 ---------- #
     # 【存在空】网络在线天数
-    network_online_days = models.CharField(max_length=255, verbose_name="网络在线天数", null=True)
+    network_online_days = models.CharField(max_length=255, verbose_name="网络在线天数", null=True, default="0")
     # 【存在空】网络总流量（GB）
-    network_flow = models.CharField(max_length=255, verbose_name="网络总流量(GB)", null=True)
+    network_flow = models.CharField(max_length=255, verbose_name="网络总流量(GB)", null=True, default="0.00")
 
     # ---------- 食堂 ---------- #
-    # 消费次数
-    consumption_times = models.CharField(max_length=10, verbose_name="消费次数")
-    # 消费金额
-    consumption_amount = models.CharField(max_length=10, verbose_name="消费金额")
+    # 【存在空】消费次数
+    consumption_times = models.CharField(max_length=255, verbose_name="消费次数", null=True, default="0")
+    # 【存在空】消费金额
+    consumption_amount = models.CharField(max_length=255, verbose_name="消费金额", null=True, default="0.00")
     # 最频繁消费地点
-    most_frequent_consumption_place = models.CharField(max_length=20, verbose_name="最频繁消费地点")
-    # 最高单次消费
-    highest_single_consumption_amount = models.CharField(max_length=10, verbose_name="最高单次消费")
-    # 最高单次消费日期
-    highest_single_consumption_date = models.DateField(verbose_name="最高单次消费日期")
+    most_frequent_consumption_place = models.CharField(max_length=255, verbose_name="最频繁消费地点", null=True)
+    # 【存在空】最高单次消费
+    highest_single_consumption_amount = models.CharField(max_length=255, verbose_name="最高单次消费", null=True,
+                                                         default="0.00")
+    # 【存在空】最高单次消费日期
+    highest_single_consumption_date = models.DateField(verbose_name="最高单次消费日期", null=True)
 
     # ---------- 课程 ---------- #
-    # 第一节课日期
-    first_class_date = models.DateField(verbose_name="第一节课日期")
-    # 第一节课地点
-    first_class_place = models.CharField(max_length=20, verbose_name="第一节课地点")
-    # 第一节课名称
-    first_class_name = models.CharField(max_length=20, verbose_name="第一节课名称")
-    # 第一节课教师姓名
-    first_class_teacher = models.CharField(max_length=20, verbose_name="第一节课教师姓名")
-    # 选课门数
-    course_selected_num = models.CharField(max_length=5, verbose_name="选课门数")
-    # 获得学分
-    credits_obtained = models.CharField(max_length=5, verbose_name="获得学分")
-    # 最高分课程名
-    highest_score_course = models.CharField(max_length=20, verbose_name="最高分课程名")
-    # 论文数量
-    papers_num = models.CharField(max_length=5, verbose_name="论文数量", default="0")
+    # 【存在空】第一节课日期
+    first_class_date = models.DateField(verbose_name="第一节课日期", null=True)
+    # 【存在空】第一节课地点
+    first_class_place = models.CharField(max_length=255, verbose_name="第一节课地点", null=True)
+    # 【存在空】第一节课名称
+    first_class_name = models.CharField(max_length=255, verbose_name="第一节课名称", null=True)
+    # 【存在空】第一节课教师姓名
+    first_class_teacher = models.CharField(max_length=255, verbose_name="第一节课教师姓名", null=True)
+    # 【存在空】选课门数
+    course_selected_num = models.CharField(max_length=255, verbose_name="选课门数", null=True, default="0")
+    # 【存在空】获得学分
+    credits_obtained = models.CharField(max_length=255, verbose_name="获得学分", null=True, default="0.00")
+    # 【存在空】最高分课程名
+    highest_score_course = models.CharField(max_length=255, verbose_name="最高分课程名", null=True)
+    # 【存在空】论文数量
+    papers_num = models.CharField(max_length=255, verbose_name="论文数量", null=True, default="0")
 
     # ---------- 人文讲座 ---------- #
-    # 参加讲座次数
-    lecture_attended_times = models.CharField(max_length=5, verbose_name="参加讲座次数", default="0")
-    # 第一次参加讲座名称
-    first_lecture_name = models.CharField(max_length=100, verbose_name="第一次参加讲座名称", null=True)
-    # 第一次参加讲座日期
+    # 【存在空】参加讲座次数
+    lecture_attended_times = models.CharField(max_length=255, verbose_name="参加讲座次数", null=True, default="0")
+    # 【存在空】第一次参加讲座名称
+    first_lecture_name = models.CharField(max_length=255, verbose_name="第一次参加讲座名称", null=True)
+    # 【存在空】第一次参加讲座日期
     first_lecture_date = models.DateField(verbose_name="第一次参加讲座日期", null=True)
 
     # ---------- SRTP ---------- #
-    # SRTP项目数
-    srtp_project_num = models.CharField(max_length=5, verbose_name="SRTP项目数", default="0")
-    # SRTP学分
-    srtp_score = models.CharField(max_length=10, verbose_name="SRTP学分", default="0")
+    # 【存在空】SRTP项目数
+    srtp_project_num = models.CharField(max_length=255, verbose_name="SRTP项目数", null=True, default="0")
+    # 【存在空】SRTP学分
+    srtp_score = models.CharField(max_length=255, verbose_name="SRTP学分", null=True, default="0.00")
 
     # ---------- 志愿活动 ---------- #
     # 志愿活动数量
-    volunteer_activity_num = models.CharField(max_length=5, verbose_name="志愿活动数量", default="0")
-    # 志愿活动时长
-    volunteer_duration = models.CharField(max_length=10, verbose_name="志愿活动时长", default="0")
+    volunteer_activity_num = models.CharField(max_length=255, verbose_name="志愿活动数量", null=True, default="0")
+    # 【存在空】志愿活动时长
+    volunteer_duration = models.CharField(max_length=255, verbose_name="志愿活动时长", null=True, default="0.00")
 
     # ---------- 社会实践 ---------- #
-    # 实践项目数量
-    practice_project_num = models.CharField(max_length=5, verbose_name="实践项目数量", default="0")
-    # 第一个实践项目
-    first_practice_project_name = models.CharField(max_length=50, verbose_name="第一个实践项目", null=True)
-    # 第一个实践项目队员（非本人的另一人姓名）
-    first_practice_project_member = models.CharField(max_length=20,
+    # 【存在空】实践项目数量
+    practice_project_num = models.CharField(max_length=255, verbose_name="实践项目数量", null=True, default="0")
+    # 【存在空】第一个实践项目
+    first_practice_project_name = models.CharField(max_length=255, verbose_name="第一个实践项目", null=True)
+    # 【存在空】第一个实践项目队员（非本人的另一人姓名）
+    first_practice_project_member = models.CharField(max_length=255,
                                                      verbose_name="第一个实践项目队员（非本人的另一人姓名）", null=True)
 
     # ---------- 图书馆 ---------- #
-    # 总借阅量
-    total_borrowed_books_num = models.CharField(max_length=10, verbose_name="总借阅量", default="0")
-    # 最长借阅天数
-    longest_book_borrowing_days = models.CharField(max_length=5, verbose_name="最长借阅天数", default="0")
-    # 个人借阅书籍中总借阅次数最少书名
-    nice_book_name = models.CharField(max_length=50, verbose_name="个人借阅书籍中总借阅次数最少书名", null=True)
-    # 个人借阅书籍中总借阅次数最少的书被多少人借过
-    nice_book_borrowing_person_num = models.CharField(max_length=100, verbose_name="个人借阅书籍中总借阅次数最少的书被多少人借过", null=True)
-    # 借阅时间最长的书名
+    # 【存在空】总借阅量
+    total_borrowed_books_num = models.CharField(max_length=255, verbose_name="总借阅量", null=True, default="0")
+    # 【存在空】最长借阅天数
+    longest_book_borrowing_days = models.CharField(max_length=255, verbose_name="最长借阅天数", null=True, default="0")
+    # 【存在空】个人借阅书籍中总借阅次数最少书名
+    nice_book_name = models.CharField(max_length=255, verbose_name="个人借阅书籍中总借阅次数最少书名", null=True)
+    # 【存在空】个人借阅书籍中总借阅次数最少的书被多少人借过
+    nice_book_borrowing_person_num = models.CharField(max_length=255,
+                                                      verbose_name="个人借阅书籍中总借阅次数最少的书被多少人借过",
+                                                      null=True)
+    # 【存在空】借阅时间最长的书名
     longest_keeping_book_name = models.CharField(max_length=255, verbose_name="借阅时间最长的书名", null=True)
 
     # ---------- 体育 ---------- #
-    # 场馆预约次数
-    gym_ordered_times = models.CharField(max_length=5, verbose_name="场馆预约次数", default="0")
-    # 首次预约场馆
-    first_ordered_gym = models.CharField(max_length=20, verbose_name="首次预约场馆", null=True)
-    # 首次预约日期
+    # 【存在空】场馆预约次数
+    gym_ordered_times = models.CharField(max_length=255, verbose_name="场馆预约次数", null=True, default="0")
+    # 【存在空】首次预约场馆
+    first_ordered_gym = models.CharField(max_length=255, verbose_name="首次预约场馆", null=True)
+    # 【存在空】首次预约日期
     first_ordered_date = models.DateField(verbose_name="首次预约日期", null=True)
-    # 最常去的场馆
-    favorite_gym = models.CharField(max_length=100, verbose_name="最常去的场馆", null=True)
-    # 最常去的场馆预约次数
-    favorite_gym_ordered_times = models.CharField(max_length=100, verbose_name="最常去的场馆预约次数", default="0")
-    # 跑操次数
-    morning_exercise_times = models.CharField(max_length=5, verbose_name="跑操次数", default="0")
-    # 最早跑操打卡时间
+    # 【存在空】最常去的场馆
+    favorite_gym = models.CharField(max_length=255, verbose_name="最常去的场馆", null=True)
+    # 【存在空】最常去的场馆预约次数
+    favorite_gym_ordered_times = models.CharField(max_length=255, verbose_name="最常去的场馆预约次数", null=True,
+                                                  default="0")
+    # 【存在空】跑操次数
+    morning_exercise_times = models.CharField(max_length=255, verbose_name="跑操次数", null=True, default="0")
+    # 【存在空】最早跑操打卡时间
     earliest_exercise_time = models.TimeField(verbose_name="最早跑操打卡时间", null=True)
 
     def __str__(self):
