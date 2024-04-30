ecfe8cff6d19fadbd9a7a46d28ef03a23af787b0
Gol3vka <gol3vka@163.com>
2024-04-30 23:48:01 +0800

    feat: add judgement functions in model

--- graduates_big_data/stat_data/models/graduate_personal_stat.py
+++ graduates_big_data/stat_data/models/graduate_personal_stat.py
@@ -1,5 +1,6 @@
-from django.db import models
 from datetime import date
+
+from django.db import models
 
 
 class GraduatePersonalStat(models.Model):
@@ -103,14 +104,14 @@ class GraduatePersonalStat(models.Model):
     total_borrowed_books_num = models.CharField(max_length=255, verbose_name="总借阅量", null=True, default="0")
     # 【存在空】借阅时间最长的书名
     longest_keeping_book_name = models.CharField(max_length=255, verbose_name="借阅时间最长的书名", null=True)
+    # 【存在空】最长借阅天数
+    longest_book_borrowing_days = models.CharField(max_length=255, verbose_name="最长借阅天数", null=True, default="0")
     # 【存在空】个人借阅书籍中总借阅次数最少书名
     nice_book_name = models.CharField(max_length=255, verbose_name="个人借阅书籍中总借阅次数最少书名", null=True)
     # 【存在空】个人借阅书籍中总借阅次数最少的书被多少人借过
     nice_book_borrowing_person_num = models.CharField(max_length=255,
                                                       verbose_name="个人借阅书籍中总借阅次数最少的书被多少人借过",
                                                       null=True)
-    # 【存在空】最长借阅天数
-    longest_book_borrowing_days = models.CharField(max_length=255, verbose_name="最长借阅天数", null=True, default="0")
 
     # ---------- 场景7: 体育馆 ---------- #
     # 【存在空】首次预约日期
@@ -138,14 +139,52 @@ class GraduatePersonalStat(models.Model):
         verbose_name_plural = verbose_name
         ordering = ["seu_card_id"]
 
-    def get_network_flow_equivalence(self):
-        return round(float(self.network_flow) / 167, 2)
+    # ----- 判断是否显示某部分文案的函数 ----- #
+    def show_same_origin_num(self):
+        return int(self.same_origin_num) > 0
+
+    def show_network_data(self):
+        return int(self.network_online_days) > 0
 
     def show_papers(self):
         return int(self.papers_num) > 0
 
+    def show_srtp_projects(self):
+        return float(self.srtp_score) > 0.0
+
+    def show_volunteer_activities(self):
+        return float(self.volunteer_duration) > 0.0
+
     def show_borrowing_details(self):
         return int(self.total_borrowed_books_num) > 0
+
+    # ----- 判断是否跳过某整个页面的函数 ----- #
+    def show_dormitory_page(self):
+        return self.dormitory_name \
+            or self.show_network_data
+
+    def show_canteen_page(self):
+        return self.most_frequent_consumption_place \
+            or self.highest_single_consumption_date
+
+    def show_auditorium_page(self):
+        return self.first_lecture_date \
+            or self.show_srtp_projects \
+            or self.show_volunteer_activities \
+            or self.first_practice_project_name
+
+    def show_library_page(self):
+        return int(self.total_borrowed_books_num) > 0 \
+            and int(self.longest_book_borrowing_days) > 0 \
+            and self.nice_book_name
+
+    def show_gym_page(self):
+        return self.favorite_gym \
+            or self.earliest_exercise_time
+
+    # ----- 用于计算的函数 ----- #
+    def get_network_flow_equivalence(self):
+        return round(float(self.network_flow) / 167, 2)
 
     def get_days_in_seu(self):
         return (date.today() - self.enroll_date).days
