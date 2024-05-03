4893aceebbf71099076fd29b65a381c9f1b41b9c
Gol3vka <gol3vka@163.com>
2024-05-03 23:55:17 +0800

    feat: update html and css

--- graduates_big_data/settings.py
+++ graduates_big_data/settings.py
@@ -29,7 +29,7 @@ ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
-ENABLE_CAS = True
+ENABLE_CAS = False
 
 INSTALLED_APPS = [
     "django.contrib.admin",
--- stat_data/views/personal_view.py
+++ stat_data/views/personal_view.py
@@ -2,7 +2,12 @@ from django.conf import settings
 from django.shortcuts import redirect, render
 
 # from django_cas_ng.decorators import login_required
-from stat_data.models import FavoriteCanteenStat, GraduatePersonalStat, LibraryBorrowingStat, SportsCompetitionStat
+from stat_data.models import (
+    FavoriteCanteenStat,
+    GraduatePersonalStat,
+    LibraryBorrowingStat,
+    SportsCompetitionStat,
+)
 
 # 显示借阅量排名的阈值（前50%才显示）
 SHOW_BORROWING_RANK_THRESHOLD = 0.5
@@ -17,7 +22,7 @@ def personal_view(request):
         if seu_card_id == "TEST_USER":
             seu_card_id = "213216666"
     else:
-        seu_card_id = "213203142"
+        seu_card_id = "213216666"
     # 若不在毕业生数据中，则显示提示信息
     if not GraduatePersonalStat.objects.filter(seu_card_id=seu_card_id).exists():
         return redirect("error:not_eligible_view")
@@ -30,46 +35,69 @@ def personal_view(request):
 
     # 最常去相同食堂的人数
     same_favorite_canteen_cnt = FavoriteCanteenStat.objects.get(
-        canteen_name=graduate.most_frequent_consumption_place).count
-    same_favorite_canteen_percent = f'{same_favorite_canteen_cnt / graduate_total_num:.2%}'
+        canteen_name=graduate.most_frequent_consumption_place
+    ).count
+    same_favorite_canteen_percent = (
+        f"{same_favorite_canteen_cnt / graduate_total_num:.2%}"
+    )
 
     # 借阅量排名
     borrowing_rank = GraduatePersonalStat.objects.filter(
-        total_borrowed_books_num=graduate.total_borrowed_books_num).count()
-    show_borrowing_rank = borrowing_rank < graduate_total_num * SHOW_BORROWING_RANK_THRESHOLD
-    borrowing_rank_percent = f'{1 - borrowing_rank / graduate_total_num:.2%}'
+        total_borrowed_books_num=graduate.total_borrowed_books_num
+    ).count()
+    show_borrowing_rank = (
+        borrowing_rank < graduate_total_num * SHOW_BORROWING_RANK_THRESHOLD
+    )
+    borrowing_rank_percent = f"{1 - borrowing_rank / graduate_total_num:.2%}"
 
     # 全校最高借阅量
-    max_total_borrowed_books_num = (LibraryBorrowingStat.objects.extra(
-        select={"total_borrowed_books_num_int": "CAST(total_borrowed_books_num AS SIGNED INTEGER)"}).order_by(
-        "-total_borrowed_books_num_int")[0]).total_borrowed_books_num
+    max_total_borrowed_books_num = (
+        LibraryBorrowingStat.objects.extra(
+            select={
+                "total_borrowed_books_num_int": "CAST(total_borrowed_books_num AS SIGNED INTEGER)"
+            }
+        ).order_by("-total_borrowed_books_num_int")[0]
+    ).total_borrowed_books_num
 
     # 本学院借阅量第一名
     unit_name = graduate.unit_name
-    top_borrower_in_unit = LibraryBorrowingStat.objects.filter(unit_name=unit_name).extra(
-        select={"total_borrowed_books_num_int": "CAST(total_borrowed_books_num AS SIGNED INTEGER)"}).order_by(
-        "-total_borrowed_books_num_int")[0]
+    top_borrower_in_unit = (
+        LibraryBorrowingStat.objects.filter(unit_name=unit_name)
+        .extra(
+            select={
+                "total_borrowed_books_num_int": "CAST(total_borrowed_books_num AS SIGNED INTEGER)"
+            }
+        )
+        .order_by("-total_borrowed_books_num_int")[0]
+    )
 
     # 场馆预约次数排名（gym_ordered_times）
     gym_ordering_rank = GraduatePersonalStat.objects.filter(
-        gym_ordered_times=graduate.gym_ordered_times).count()
-    gym_ordering_rank_percent = f'{1 - gym_ordering_rank / graduate_total_num:.2%}'
+        gym_ordered_times=graduate.gym_ordered_times
+    ).count()
+    gym_ordering_rank_percent = f"{1 - gym_ordering_rank / graduate_total_num:.2%}"
 
     # 是否显示体育竞赛
-    sports_competition_list = SportsCompetitionStat.objects.filter(seu_card_id=seu_card_id)
+    sports_competition_list = SportsCompetitionStat.objects.filter(
+        seu_card_id=seu_card_id
+    )
     show_sports_competitions = bool(sports_competition_list)
-    sports_competition = sports_competition_list[0] if show_sports_competitions else None
+    sports_competition = (
+        sports_competition_list[0] if show_sports_competitions else None
+    )
 
-    return render(request,
-                  "personal_view.html",
-                  {
-                      "graduate": graduate,
-                      "same_favorite_canteen_percent": same_favorite_canteen_percent,
-                      "show_borrowing_rank": show_borrowing_rank,
-                      "borrowing_rank_percent": borrowing_rank_percent,
-                      "max_total_borrowed_books_num": max_total_borrowed_books_num,
-                      "top_borrower_in_unit": top_borrower_in_unit,
-                      "gym_ordering_rank_percent": gym_ordering_rank_percent,
-                      "show_sports_competitions": show_sports_competitions,
-                      "sports_competition": sports_competition
-                  })
+    return render(
+        request,
+        "personal_view.html",
+        {
+            "graduate": graduate,
+            "same_favorite_canteen_percent": same_favorite_canteen_percent,
+            "show_borrowing_rank": show_borrowing_rank,
+            "borrowing_rank_percent": borrowing_rank_percent,
+            "max_total_borrowed_books_num": max_total_borrowed_books_num,
+            "top_borrower_in_unit": top_borrower_in_unit,
+            "gym_ordering_rank_percent": gym_ordering_rank_percent,
+            "show_sports_competitions": show_sports_competitions,
+            "sports_competition": sports_competition,
+        },
+    )
