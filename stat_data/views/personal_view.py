00c1f646ec090466f70dab9b7e28754f878adb33
Gol3vka <gol3vka@163.com>
2024-05-05 22:25:11 +0800

    style: format

--- stat_data/views/personal_view.py
+++ stat_data/views/personal_view.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.shortcuts import redirect, render
 
-# from django_cas_ng.decorators import login_required
+from django_cas_ng.decorators import login_required
 from stat_data.models import (
     FavoriteCanteenStat,
     GraduatePersonalStat,
@@ -13,7 +13,7 @@ from stat_data.models import (
 SHOW_BORROWING_RANK_THRESHOLD = 0.5
 
 
-# @login_required  # TODO: 生产环境中启用
+@login_required  # TODO: 生产环境中启用
 def personal_view(request):
     # 获取当前用户的一卡通号
     if settings.ENABLE_CAS:
@@ -22,7 +22,7 @@ def personal_view(request):
         if seu_card_id == "TEST_USER":
             seu_card_id = "213216666"
     else:
-        seu_card_id = "213203227"
+        seu_card_id = "213216666"
     # 若不在毕业生数据中，则显示提示信息
     if not GraduatePersonalStat.objects.filter(seu_card_id=seu_card_id).exists():
         return redirect("error:not_eligible_view")
