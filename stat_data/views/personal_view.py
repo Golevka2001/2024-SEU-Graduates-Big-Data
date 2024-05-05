7b3e02162bb3ebf44d2f9f4d931a8d9d5749486c
Gol3vka <gol3vka@163.com>
2024-05-05 22:23:30 +0800

    fix: errors of the database query part

--- stat_data/views/personal_view.py
+++ stat_data/views/personal_view.py
@@ -22,7 +22,7 @@ def personal_view(request):
         if seu_card_id == "TEST_USER":
             seu_card_id = "213216666"
     else:
-        seu_card_id = "213216666"
+        seu_card_id = "213203227"
     # 若不在毕业生数据中，则显示提示信息
     if not GraduatePersonalStat.objects.filter(seu_card_id=seu_card_id).exists():
         return redirect("error:not_eligible_view")
@@ -34,19 +34,22 @@ def personal_view(request):
     graduate_total_num = GraduatePersonalStat.objects.count()
 
     # 最常去相同食堂的人数
-    same_favorite_canteen_cnt = FavoriteCanteenStat.objects.get(
-        canteen_name=graduate.most_frequent_consumption_place
-    ).count
-    same_favorite_canteen_percent = (
-        f"{same_favorite_canteen_cnt / graduate_total_num:.2%}"
-    )
+    if not graduate.most_frequent_consumption_place:
+        same_favorite_canteen_percent = "0.00%"
+    else:
+        same_favorite_canteen_cnt = FavoriteCanteenStat.objects.get(
+            canteen_name=graduate.most_frequent_consumption_place
+        ).count
+        same_favorite_canteen_percent = (
+            f"{same_favorite_canteen_cnt / graduate_total_num:.2%}"
+        )
 
     # 借阅量排名
     borrowing_rank = GraduatePersonalStat.objects.filter(
         total_borrowed_books_num=graduate.total_borrowed_books_num
     ).count()
     show_borrowing_rank = (
-        borrowing_rank < graduate_total_num * SHOW_BORROWING_RANK_THRESHOLD
+            borrowing_rank < graduate_total_num * SHOW_BORROWING_RANK_THRESHOLD
     )
     borrowing_rank_percent = f"{1 - borrowing_rank / graduate_total_num:.2%}"
 
@@ -60,16 +63,20 @@ def personal_view(request):
     ).total_borrowed_books_num
 
     # 本学院借阅量第一名
-    unit_name = graduate.unit_name
-    top_borrower_in_unit = (
-        LibraryBorrowingStat.objects.filter(unit_name=unit_name)
-        .extra(
-            select={
-                "total_borrowed_books_num_int": "CAST(total_borrowed_books_num AS SIGNED INTEGER)"
-            }
+    if not graduate.unit_name:
+        top_borrower_in_unit = None
+    else:
+        unit_name = graduate.unit_name
+        same_unit_borrowers = (
+            LibraryBorrowingStat.objects.filter(unit_name=unit_name)
+            .extra(
+                select={
+                    "total_borrowed_books_num_int": "CAST(total_borrowed_books_num AS SIGNED INTEGER)"
+                }
+            )
+            .order_by("-total_borrowed_books_num_int")
         )
-        .order_by("-total_borrowed_books_num_int")[0]
-    )
+        top_borrower_in_unit = same_unit_borrowers[0] if same_unit_borrowers else None
 
     # 场馆预约次数排名（gym_ordered_times）
     gym_ordering_rank = GraduatePersonalStat.objects.filter(
