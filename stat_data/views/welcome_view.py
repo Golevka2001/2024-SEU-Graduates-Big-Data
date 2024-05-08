fdfc3c95ea4718bd3736adf4b57752c3fe3665b1
Gol3vka <gol3vka@163.com>
2024-05-08 17:51:35 +0800

    style: format

--- stat_data/views/welcome_view.py
+++ stat_data/views/welcome_view.py
@@ -8,7 +8,7 @@ def welcome_view(request):
     # 若用户未通过 CAS 认证，则跳转至 CAS 登录页面
     if not request.user.is_authenticated or not request.user.username:
         return LoginView.as_view()(request)
-    
+
     # 检查是否为本届毕业生，若不在毕业生数据中，则跳转到错误页面，显示仅对本届毕业生开放的提示
     seu_card_id = request.user.username
     if not GraduatePersonalStat.objects.filter(seu_card_id=seu_card_id).exists():
