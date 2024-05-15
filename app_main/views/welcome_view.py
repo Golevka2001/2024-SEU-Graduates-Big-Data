097cfb895267380197d0ebdcb8ad81429822ce98
Gol3vka <gol3vka@163.com>
2024-05-15 22:59:56 +0800

    fix: query error

--- app_main/views/welcome_view.py
+++ app_main/views/welcome_view.py
@@ -12,10 +12,14 @@ def welcome_view(request):
     # 检查是否为本届毕业生，若不在毕业生数据中，则跳转到错误页面，显示仅对本届毕业生开放的提示
     seu_card_id = request.user.username
     is_eligible = True
+    graduate_name = None
     if not GraduatePersonalStat.objects.filter(seu_card_id=seu_card_id).exists():
         is_eligible = False
+    else:
+        graduate_name = GraduatePersonalStat.objects.get(
+            seu_card_id=seu_card_id
+        ).full_name
 
-    graduate_name = GraduatePersonalStat.objects.get(seu_card_id=seu_card_id).full_name
     return render(
         request,
         "welcome_view.html",
